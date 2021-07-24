8b0ac7ef0875907abc3f7fac35c87e724fcdad30
EugenNaydyon <eugennaydyon@gmail.com>
2021-07-24 20:49:21 +0300

    work with Themes_In process

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+// === Switching of themes ===
+// 1. Get the links
+const iconTheme = document.querySelector('#theme-switch-toggle');
+console.log(iconTheme);
+
+const bodyClass = document.querySelector('body');
+console.log(bodyClass.classList);
+
+iconTheme.addEventListener('change', onClickIconTheme);
+
+function onClickIconTheme(evt) {
+  console.log('Слушатель работает');
+  bodyClass.classList.add('.dark-theme');
+  //   if (bodyClass.classList.contains('.light-theme')) {
+  //     bodyClass.classList.add('.dark-theme');
+  //   }
+}
